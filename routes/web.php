7862b178b8e12cb88624845115676c48fd75dfc9
kawamuratakaki <takaki.kawamura0210@gmail.com>
2023-12-04 00:38:41 +0900

    ブログ投稿作成画面の作成

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,6 @@ Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/', [PostController::class, 'index']);
 
+Route::get('/posts/create', [PostController::class, 'create']);
+
 Route::get('/posts/{post}', [PostController::class ,'show']);
